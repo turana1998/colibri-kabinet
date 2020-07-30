bffc79f55d54041d45bb8555369bdb7f56f9e3ec
turana1998 <turana221298@gmail.com>
2020-07-30 14:24:25 +0400

    up

--- style/mystyle/js/my_style.js
+++ style/mystyle/js/my_style.js
@@ -16,66 +16,62 @@ $(document).on('ready', function () {
   });
 
 });
-let navStatus=false;
-$( "#mobile-toggle" ).click(function() {
-  if(navStatus){
-  $( ".mobile-navbar" ).animate({
-    left: "-204px",
-  }, 1500 );
-  navStatus=false;
-   document.getElementById("overlay").style.display = "none";
-  $("#nav-button").removeClass();
-   $("#nav-button").addClass("fas fa-bars ml-1")
-}
-else
-{
- $( ".mobile-navbar" ).animate({
-    left: "0px",
-  }, 1500 );
-  navStatus=true;
-  document.getElementById("overlay").style.display = "block";
-   $("#nav-button").removeClass();
-   $("#nav-button").addClass("fa fa-times ml-1")
-   $( ".private-account-nav" ).animate({
-    right: "-240px",
-    display:"none"
-  }, 1500 );
-
-  rightnavStatus=false;
-   setTimeout(function() { 
-       $( ".private-account-nav" ).hide();
-    }, 1400);
-}
-});
-let rightnavStatus=false;
-$( "#right-nav" ).click(function() {
-  if(rightnavStatus){
-  $( ".private-account-nav" ).animate({
-    right: "-240px",
-    display:"none"
-  }, 1500 );
-
-  rightnavStatus=false;
-  document.getElementById("overlay").style.display = "none";
-   setTimeout(function() { 
-       $( ".private-account-nav" ).hide();
-    }, 1400);
-}
-else
-{
-   $( ".private-account-nav" ).show();
- $( ".private-account-nav" ).animate({
-    right: "0px",
-    display:"block"
-  }, 1500 );
-   rightnavStatus=true;
-  
-   $( ".mobile-navbar" ).animate({
-    left: "-204px",
-  }, 1500 );
-  navStatus=false;
-  document.getElementById("overlay").style.display = "block";
+let navStatus = false;
+$("#mobile-toggle").click(function () {
+  if (navStatus) {
+    $(".mobile-navbar").animate({
+      left: "-204px",
+    }, 1500);
+    navStatus = false;
+    document.getElementById("overlay").style.display = "none";
     $("#nav-button").removeClass();
-   $("#nav-button").addClass("fas fa-bars ml-1")
-}
+    $("#nav-button").addClass("fas fa-bars ml-1")
+  } else {
+    $(".mobile-navbar").animate({
+      left: "0px",
+    }, 1500);
+    navStatus = true;
+    document.getElementById("overlay").style.display = "block";
+    $("#nav-button").removeClass();
+    $("#nav-button").addClass("fa fa-times ml-1")
+    $(".private-account-nav").animate({
+      right: "-240px",
+      display: "none"
+    }, 1500);
+
+    rightnavStatus = false;
+    setTimeout(function () {
+      $(".private-account-nav").hide();
+    }, 1400);
+  }
+});
+let rightnavStatus = false;
+$("#right-nav").click(function () {
+  if (rightnavStatus) {
+    $(".private-account-nav").animate({
+      right: "-240px",
+      display: "none"
+    }, 1500);
+
+    rightnavStatus = false;
+    document.getElementById("overlay").style.display = "none";
+    setTimeout(function () {
+      $(".private-account-nav").hide();
+    }, 1400);
+  } else {
+    $(".private-account-nav").show();
+    $(".private-account-nav").animate({
+      right: "0px",
+      display: "block"
+    }, 1500);
+    rightnavStatus = true;
+
+    $(".mobile-navbar").animate({
+      left: "-204px",
+    }, 1500);
+    navStatus = false;
+    document.getElementById("overlay").style.display = "block";
+    $("#nav-button").removeClass();
+    $("#nav-button").addClass("fas fa-bars ml-1")
+  }
 });
